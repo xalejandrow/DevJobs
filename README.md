26d98bb352de0983ef0c05e974f1f98a95d2816b
Alew <xalejandrow@gmail.com>
2021-09-24 19:53:42 -0300

    Cap 109 - Habilitando Handlebars como Template Engine

---
+++ README.md
@@ -0,0 +1,9 @@
+Sección 28: devJobs - Plataforma para Encontrar y publicar trabajos para desarrolladores
+
+Para iniciar el Proyecto:
+
+    npm init
+    npm install --save express express-handlebars
+    
+Para no tener que reiniciar el servidor y se reinicie solo para tomar los cambios
+    npm install --save-dev nodemon
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const router = require('./routes');
 
 const app = express();
+
+// habilitar handlebars como view
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+// statics files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router());
 
