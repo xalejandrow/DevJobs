b401cc4cb996d72649ddf6b6b9dd8a70a10a874a
Alew <xalejandrow@gmail.com>
2021-09-24 19:40:24 -0300

    Sec. - 28 Cap. - 108. Creando el Router

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const router = require('./routes');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('Funciona');
-})
+app.use('/', router());
 
 app.listen(5000);
