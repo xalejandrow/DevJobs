92debcd26f016a58a7f41ef9a61200a00cf262ed
Alew <xalejandrow@gmail.com>
2021-09-29 19:39:59 -0300

    Cap. 131. Agregar los Skills al input hidden

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if(skills){
         skills.addEventListener('click', agregarSkills);
+
+        // una vez que estamos en editar, llamar la función
+        skillsSeleccionados();
     }
 })
 
@@ -23,4 +26,17 @@ const agregarSkills = e => {
 
     const skillsArray = [...skills]
     document.querySelector('#skills').value = skillsArray
+}
+
+const skillsSeleccionados = () => {
+    const seleccionadas = Array.from(document.querySelectorAll('.lista-conocimientos .activo'));
+    console.log(seleccionadas);
+
+    seleccionadas.forEach(seleccionada => {
+        skills.add(seleccionada.textContent);
+    })
+    // inyectarlo en el hidden
+    const skillsArray = [...skills]
+    document.querySelector('#skills').value = skillsArray
+    
 }
