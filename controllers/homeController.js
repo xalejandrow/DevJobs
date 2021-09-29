a8caecf24481038445276e1cbd99689b68ce9767
Alew <xalejandrow@gmail.com>
2021-09-29 18:37:08 -0300

    Cap. 126. Mostrando las vacantes en la página principal

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,18 @@
-exports.mostrarTrabajos = (req, res) => {
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
+
+exports.mostrarTrabajos = async (req, res, next) => {
+
+    const vacantes = await Vacante.find().lean();
+
+    console.log(vacantes);
+    if(!vacantes) return next();
+
     res.render('home', {
         nombrePagina : 'devJobs',
         tagline : 'Encuentra y Publica trabajos para Desarrolladores Web',
         barra : true,
         boton : true,
+        vacantes
     });
 }
