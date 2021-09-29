99282d2b5df9e4006e78aad86ed43a6902b7f2fc
Alew <xalejandrow@gmail.com>
2021-09-29 19:30:10 -0300

    Cap. 130. Finalizando el Formulario para editar las vacantes

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,11 +1,15 @@
 module.exports = {
     seleccionarSkills : (seleccionadas = [], opciones) => {
+
+        console.log(seleccionadas);
+        // console.log(opciones.fn());
+
         const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox', 'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS', 'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP', 'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize', 'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'];
 
         let html = '';
         skills.forEach(skill => {
             html += `
-                <li>${skill}</li>
+                <li ${seleccionadas.includes(skill)? 'class="activo"' : ''}>${skill}</li>
             `;
         });
 
