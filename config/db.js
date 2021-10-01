05440e49787fe6f6d6422598f91992e8f967dfde
Alew <xalejandrow@gmail.com>
2021-10-01 11:42:43 -0300

    Cap. 135. Creando el Modelo de Reclutadores (Usuarios)

--- config/db.js
+++ config/db.js
@@ -7,4 +7,5 @@ mongoose.connection.on('error',(error) => {
     console.log(error);
 })
 // importar los modelos
+require('../models/Usuarios');
 require('../models/Vacantes');
---
+++ models/Usuarios.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const bcrypt = require('bcrypt');
+
+const usuariosSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    token: String,
+    expira: Date
+});
+
+// Método para hashearlos passwords
+usuariosSchema.pre('save', async function(next) {
+    // si el password ya está hasheado
+    if(!this.isModified('password')){
+        return next(); //deten la ejecución
+    }
+    // si no está hasheado
+    const hash = await bcrypt.hash(this.password, 12);
+    this.password = hash;
+    next();
+})
+
+module.exports = mongoose.model('Usuarios', usuariosSchema);
