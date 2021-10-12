3d40c0fad3e8e2a9373da89f62496b6c8358e532
Alew <xalejandrow@gmail.com>
2021-10-12 18:46:25 -0300

    Cap. 165. Mostrando la Imagen junto a las vacantes

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -31,8 +31,8 @@ exports.agregarVacante = async (req, res) => {
 }
 
 exports.mostrarVacante = async (req, res, next) => {
-    const vacante = await Vacante.findOne({ url: req.params.url }).lean();
-
+    const vacante = await Vacante.findOne({ url: req.params.url }).lean().populate('autor');
+    // console.log(vacante);
     // si no hay resultados
     if (!vacante) return next();
 
